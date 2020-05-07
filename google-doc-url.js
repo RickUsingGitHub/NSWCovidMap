2aa0100849422fcaad4032b82516fd0c9eea0c22
RickUsingGitHub <64953373+RickUsingGitHub@users.noreply.github.com>
2020-05-07 20:12:48 +1000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'xcvxcvhttps://docs.google.com/spreadsheets/d/12klG2YjY2QFNbTLxaEkQPxwz_7O75iSthzMcLFcdNlE/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/12klG2YjY2QFNbTLxaEkQPxwz_7O75iSthzMcLFcdNlE/edit#gid=0';
