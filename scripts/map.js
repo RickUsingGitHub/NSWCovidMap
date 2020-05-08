d53f514874976c7a9cd99841a0bfb4852531dc56
RickUsingGitHub <64953373+RickUsingGitHub@users.noreply.github.com>
2020-05-09 09:54:38 +1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -128,7 +128,7 @@ $(window).on('load', function() {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Date'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Group']+'<br>'+point['Where']);
+          point['Group']+'<br>'+point['Where'], {offset: [0,30]});
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
