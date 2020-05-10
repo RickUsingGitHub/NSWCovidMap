cafee78c7d5533df836d6b821a8f3695e03c188d
RickUsingGitHub <64953373+RickUsingGitHub@users.noreply.github.com>
2020-05-10 12:55:06 +1000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,8 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/12klG2YjY2QFNbTLxaEkQPxwz_7O75iSthzMcLFcdNlE/edit#gid=0';
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+if (urlParams.has('gdoc')){
+	googleDocURL = urlParams.get('gdoc');
+}
